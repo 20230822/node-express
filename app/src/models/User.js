01e449965d516c3cb226c53588ee8ca9ceac16d0
ysy56 <ysy561356@naver.com>
2023-09-21 17:27:30 +0900

    feat : 회원가입 글자수 제한 및 아이디 비밀번호 확인

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -28,8 +28,24 @@ class User{
     async register(){
         const client  = this.body;
         try{
-            const response =await UserStorage.save(client);
-            return response;
+            // 글자수 제한 확인
+            if (client.id.length < 6 || client.id.length > 40) {
+                return { success: false, mag: "아이디는 6~40자로 설정하셔야 합니다." }
+            }
+            if (client.psword.length < 8 || client.psword.length > 40) {
+                return { success: false, msg: "비밀번호는 8~40자로 설정하셔야 합니다." }
+            }
+
+            // 아이디 존재 여부, 비밀번호 같은지 확인
+            const response = await UserStorage.getUserInfo(client.id);
+            if (!response) {
+                if (client.psword == client['confirm-psword']) {
+                    const response = await UserStorage.save(client);
+                    return response;
+                }
+                return { success: false, msg : "비밀번호가 다릅니다." };
+            }
+            return { success: false, msg : "이미 존재하는 아이디입니다." };
         }catch(err){
             throw(err);
 
