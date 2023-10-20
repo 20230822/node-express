f5a9d8c34dc6da9c354fb6bc12429dd5c16f93a0
realcold0 <realcold0@gmail.com>
2023-10-20 21:39:07 +0900

    CORS오류 수정

--- app/app.js
+++ app/app.js
@@ -36,8 +36,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin : 'http://localhost:3000',
-  methods : ['GET', 'POST', 'PUT'],
+  origin : process.env.SERVER_HOST,
+  methods : ['GET', 'POST', 'PUT', 'DELETE'],
   credentials : true //쿠키 사용
 }));
 
