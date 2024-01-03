f6ca561011944be326e6430356d015619ddb9a71
realcold0 <realcold0@gmail.com>
2024-01-04 01:23:52 +0900

    fix : brycpt 오류 수정

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { response } = require('express');
 const UserStorage = require('./UserStorage');
 
